d631c699370d7aa60b93fa5965236417fa2d4c66
Romain Pellerin <rpellerin@users.noreply.github.com>
2023-01-03 12:12:36 +0100

    Update door-sensor.py

--- door-sensor.py
+++ door-sensor.py
@@ -6,6 +6,8 @@ import RPi.GPIO as GPIO
 import time
 import sys, os
 import signal
+import redis
+import datetime
 
 DOOR_SENSOR_PIN = 18
 
@@ -28,17 +30,24 @@ GPIO.setup(DOOR_SENSOR_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 # Set the cleanup handler for when user hits Ctrl-C to exit
 signal.signal(signal.SIGINT, cleanup)
 
+r = redis.Redis()
+
 print("Listening to the door state change...")
 
 while True:
     oldIsOpen = isOpen
     isOpen = GPIO.input(DOOR_SENSOR_PIN)
 
-    if (isOpen and (isOpen != oldIsOpen)):
-        print("Door is open")
-        os.system('echo "Hello, friend." | mail -s "Door opened!" root@localhost')
-    elif (isOpen != oldIsOpen):
-        print("Door is closed")
-        os.system('echo "Hello, friend." | mail -s "Door closed" root@localhost')
+    if (isOpen != oldIsOpen):
+        utc_offset_in_hours = int(-time.timezone/3600)
+        now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=utc_offset_in_hours))).strftime('%d/%m/%Y %H:%M:%S')
+        if (isOpen):
+            print("Door is open")
+            r.rpush('door_status', str({ 'timestamp': now, 'status': 'open', 'old_is_open': oldIsOpen}))
+            os.system('echo "Hello, friend." | mail -s "Door opened!" root@localhost')
+        else:
+            print("Door is closed")
+            r.rpush('door_status', str({ 'timestamp': now, 'status': 'closed', 'old_is_open': oldIsOpen}))
+            os.system('echo "Hello, friend." | mail -s "Door closed" root@localhost')
 
     time.sleep(0.1)
