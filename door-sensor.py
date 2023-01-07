a8b51b38c03fa30f47e27511573a2bbcf6d589b1
Romain Pellerin <rpellerin@users.noreply.github.com>
2023-01-07 19:02:35 +0100

    Minor edits

--- door-sensor.py
+++ door-sensor.py
@@ -8,6 +8,9 @@ import sys, os
 import signal
 import redis
 import datetime
+import requests
+
+url="https://script.google.com/macros/s/XYZ/exec"
 
 DOOR_SENSOR_PIN = 18
 
@@ -17,6 +20,19 @@ GPIO.setmode(GPIO.BCM)
 # Initially we don't know if the door sensor is open or closed...
 isOpen = None
 oldIsOpen = None
+
+def send_request(data):
+    try:
+        response = requests.get(
+            url,
+            params={
+                "datetime": data['timestamp'],
+                "door_status": data['door_status'],
+            },
+        )
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
 
 # Clean up when the user exits with keyboard interrupt
 def cleanup(signal, frame):
@@ -41,13 +57,22 @@ while True:
     if (isOpen != oldIsOpen):
         utc_offset_in_hours = int(-time.timezone/3600)
         now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=utc_offset_in_hours))).strftime('%d/%m/%Y %H:%M:%S')
-        if (isOpen):
-            print("Door is open")
-            r.rpush('door_status', str({ 'timestamp': now, 'status': 'open', 'old_is_open': oldIsOpen}))
-            os.system('echo "Hello, friend." | mail -s "Door opened!" root@localhost')
-        else:
-            print("Door is closed")
-            r.rpush('door_status', str({ 'timestamp': now, 'status': 'closed', 'old_is_open': oldIsOpen}))
-            os.system('echo "Hello, friend." | mail -s "Door closed" root@localhost')
+        door_status = 'open' if isOpen else 'closed'
+        data = { 'timestamp': now, 'door_status': door_status }
+
+        successfully_sent = False
+        try:
+            successfully_sent = send_request(data)
+        except BaseException as e:
+            print("Error: %s" % str(e))
+
+        if not successfully_sent:
+            print('Failed to post to Google App Script')
+            print(data)
+            r.rpush('door_status', str(data))
+            if (isOpen):
+                os.system('echo "Hello, friend." | mail -s "Door opened!" root@localhost')
+            else:
+                os.system('echo "Hello, friend." | mail -s "Door closed" root@localhost')
 
     time.sleep(0.1)
