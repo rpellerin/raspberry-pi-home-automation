d3bc20eca60d575a2b45439c5256b0b39087ec17
Romain Pellerin <rpellerin@users.noreply.github.com>
2023-01-31 00:25:52 +0100

    Add sync to video-to-email

--- video-to-email.sh
+++ video-to-email.sh
@@ -8,5 +8,7 @@ raspivid -a 12 -w 1296 -h 972 -hf -vf -fps 25 -t 15000 -b 3000000 -o "/tmp/$file
 # New way of recording
 #libcamera-vid --width 1296 --height 972 --hflip --vflip --framerate 25 -t 15000 -b 3000000 -o "/tmp/$filename.h264" # 3000k bits per second (300kB/s)
 
+sync
 ffmpeg -r 25 -i "/tmp/$filename.h264" -vcodec copy "/tmp/$filename.mp4"
+sync
 echo "On `date`" | mail -s "Door opened" -A "/tmp/$filename.mp4" root@localhost
