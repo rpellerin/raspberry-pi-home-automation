0acf5e86f1b12089c3fc13a04045c376b7123c53
Romain Pellerin <rpellerin@users.noreply.github.com>
2023-03-28 23:42:42 +0200

    Update report_weather.py

--- report_weather.py
+++ report_weather.py
@@ -2,7 +2,6 @@ import smbus2
 import bme280
 import time
 import requests
-import datetime
 import sys
 import redis
 import json
@@ -16,7 +15,7 @@ port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
 utc_offset_in_hours = int(-time.timezone/3600)
-now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=utc_offset_in_hours))).strftime('%d/%m/%Y %H:%M:%S')
+now = time.strftime('%d/%m/%Y %H:%M:%S', time.localtime())
 
 calibration_params = bme280.load_calibration_params(bus, address)
 def send_request(data):
