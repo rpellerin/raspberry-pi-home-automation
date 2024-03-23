acb971263eb74cb6f472e2527b5e234c94c43746
Romain Pellerin <rpellerin@users.noreply.github.com>
2024-03-23 23:09:46 +0100

    Minor changes

--- config.py
+++ config.py
@@ -3,10 +3,8 @@ import os
 
 repo_path = os.path.dirname(os.path.realpath(__file__))
 
-def get_project_root():
-    return repo_path
 
 def get_config():
     config = configparser.ConfigParser()
-    config.read_file(open(repo_path + '/config.txt'))
+    config.read_file(open(repo_path + "/config.txt"))
     return config
