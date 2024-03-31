9cb55b790b453214b360d0a25944654c5a3ab1c6
Romain Pellerin <rpellerin@users.noreply.github.com>
2024-03-31 18:19:32 +0200

    Test the CLI

--- home_automation/__main__.py
+++ home_automation/__main__.py
@@ -1,22 +1,24 @@
 import sys
-from .report_weather import send_report
-from .remote_control import run as remote_control
-from .build_arduino_sketch_and_deploy import build_and_deploy
-
 
 if __name__ == "__main__":
     action = sys.argv[1] if len(sys.argv) >= 2 else None
 
     if action == None:
-        print("No action given.")
+        print("No action given.", file=sys.stderr)
         sys.exit(1)
 
     success = False
     if action == "report_weather":
+        from .report_weather import send_report  # TODO: test these lazy imports
+
         success = send_report()
     elif action == "remote_control":
+        from .remote_control import run as remote_control
+
         success = remote_control()
     elif action == "build_arduino_sketch_and_deploy":
+        from .build_arduino_sketch_and_deploy import build_and_deploy
+
         success = build_and_deploy()
     else:
         print(f"Unknown action: {action}", file=sys.stderr)
